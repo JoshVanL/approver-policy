b4f9378ab9ba1c62262439b59170333a6762b275
joshvanl <vleeuwenjoshua@gmail.com>
2021-08-12 00:17:41 +0100

    Move logic from /cmd/app to /internal/cmd
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -17,82 +17,18 @@ limitations under the License.
 package app
 
 import (
-	"context"
 	"fmt"
 	"os"
 
-	"github.com/spf13/cobra"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/healthz"
 
-	"github.com/cert-manager/policy-approver/cmd/app/options"
-	"github.com/cert-manager/policy-approver/pkg/api"
-	"github.com/cert-manager/policy-approver/pkg/controllers"
-	"github.com/cert-manager/policy-approver/pkg/policy"
-)
-
-const (
-	helpOutput = "A cert-manager policy approver which bases decisions on CertificateRequestPolicies"
+	"github.com/cert-manager/policy-approver/internal/cmd"
 )
 
 func ExecutePolicyApprover() {
-	cmd := newCommand(ctrl.SetupSignalHandler())
+	cmd := cmd.NewCommand(ctrl.SetupSignalHandler())
 	if err := cmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
-}
-
-func newCommand(ctx context.Context) *cobra.Command {
-	opts := new(options.Options)
-
-	cmd := &cobra.Command{
-		Use:   "policy-approver",
-		Short: helpOutput,
-		Long:  helpOutput,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			opts.Complete()
-			log := opts.Log
-
-			mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-				Scheme:                  api.Scheme,
-				MetricsBindAddress:      opts.MetricsAddress,
-				HealthProbeBindAddress:  opts.ProbeAddress,
-				LeaderElectionNamespace: opts.LeaderElectionNamespace,
-				LeaderElection:          true,
-				LeaderElectionID:        "policy.cert-manager.io",
-				Logger:                  log,
-			})
-			if err != nil {
-				log.Error(err, "unable to start manager")
-				os.Exit(1)
-			}
-
-			c := controllers.New(
-				ctrl.Log, mgr.GetClient(),
-				mgr.GetEventRecorderFor("policy-approver"),
-				policy.New(mgr.GetClient(), opts.ApproveWhenNoPolicies),
-			)
-			if err := c.SetupWithManager(mgr); err != nil {
-				log.Error(err, "unable to create controller", "controller", "CertificateRequestPolicy")
-				os.Exit(1)
-			}
-
-			if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
-				log.Error(err, "unable to set up health check")
-				os.Exit(1)
-			}
-			if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
-				log.Error(err, "unable to set up ready check")
-				os.Exit(1)
-			}
-
-			log.Info("starting manager")
-			return mgr.Start(ctx)
-		},
-	}
-
-	opts.AddFlags(cmd)
-
-	return cmd
 }
